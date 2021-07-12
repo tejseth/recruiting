27c4d937e079b4b366c86f5279d22f06dfdac227
tejseth <tejseth@umich.edu>
2021-07-12 09:00:20 -0400

    loading in the data and cleaning it

---
+++ loading-cleaning-data.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+
+recruiting_247_2010 <- read.csv("~/recruiting/247_recruiting/recruiting_247_2010.csv")
+recruiting_247_2011 <- read.csv("~/recruiting/247_recruiting/recruiting_247_2011.csv")
+recruiting_247_2012 <- read.csv("~/recruiting/247_recruiting/recruiting_247_2012.csv")
+recruiting_247_2013 <- read.csv("~/recruiting/247_recruiting/recruiting_247_2013.csv")
+recruiting_247_2014 <- read.csv("~/recruiting/247_recruiting/recruiting_247_2014.csv")
+recruiting_247_2015 <- read.csv("~/recruiting/247_recruiting/recruiting_247_2015.csv")
+recruiting_247_2016 <- read.csv("~/recruiting/247_recruiting/recruiting_247_2016.csv")
+recruiting_247_2017 <- read.csv("~/recruiting/247_recruiting/recruiting_247_2017.csv")
+recruiting_247_2018 <- read.csv("~/recruiting/247_recruiting/recruiting_247_2018.csv")
+recruiting_247_2019 <- read.csv("~/recruiting/247_recruiting/recruiting_247_2019.csv")
+recruiting_247_2020 <- read.csv("~/recruiting/247_recruiting/recruiting_247_2020.csv")
+
+recruiting_data <- do.call("rbind", list(recruiting_247_2010, recruiting_247_2011, recruiting_247_2012,
+                                         recruiting_247_2013, recruiting_247_2014, recruiting_247_2015,
+                                         recruiting_247_2016, recruiting_247_2017, recruiting_247_2018,
+                                         recruiting_247_2019, recruiting_247_2020))
+
+NCAA_WAA_6_12 <- read_csv("247_recruiting/NCAA_WAA_6_12.csv")
+
+recruiting_data_filtered <- recruiting_data %>%
+  filter(!is.na(data.rating))
+
+total_ncaa_waa <- NCAA_WAA_6_12 %>%
+  group_by(player, player_id) %>%
+  summarize(seasons = n(),
+            total_waa = sum(WAA),
+            waa_per_season = total_waa / seasons,
+            total_snaps = sum(total_snaps),
+            snaps_per_season = total_snaps / seasons)
+
+recruiting_data_filtered <- recruiting_data_filtered %>%
+  left_join(total_ncaa_waa, by = c("data.player_id" = "player_id"))
+
+recruiting_with_waa <- recruiting_data_filtered %>%
+  filter(!is.na(total_waa))
+
+recruiting_with_waa_season <- recruiting_with_waa %>%
+  filter(seasons > 1)
